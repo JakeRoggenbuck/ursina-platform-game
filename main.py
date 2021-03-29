5bd1787eba3588551f12ee9c0956fc64c5d14808
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-03-29 01:18:07 -0700

    Add some more interesting things

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import random
-from ursina import *
+import ursina as urs
 from ursina.prefabs.first_person_controller import FirstPersonController
 
 
-app = Ursina()
+app = urs.Ursina()
 
 
-class Platform(Entity):
+class Platform(urs.Entity):
     def __init__(self, x=0, y=2, z=0):
-        self.z = z
-        self.y = y
         super().__init__(
-            parent=scene,
+            parent=urs.scene,
             model='cube',
+            texture='white_cube',
             collider='box',
-            color=color.hex("#21a5f5"),
+            color=urs.color.hex("#21a6f6"),
             scale=(20, 1, 20),
             position=(x, y, z),
         )
@@ -24,18 +23,32 @@ class Platforms:
     def __init__(self):
         self.platforms = []
         self.x, self.y, self.z = 0, 0, 0
+        self.x_delta, self.z_delta = 0, 0
 
     def add(self):
         platform = Platform(self.x, self.y, self.z)
-        self.x = random.randint(-10, 10)
+
+        # Changes the platforms location
+        self.x = random.randint(-10 - self.x_delta, 10 + self.x_delta)
         self.y -= 10
-        self.z += 50
+        self.z += 50 + self.z_delta
+
         self.platforms.append(platform)
+        # How much extra challenge is added, [0, 0, ..., 0, 1]
+        # There is a low change the delta increases
+        self.x_delta += random.choice([*[0] * 9, 1])
+        self.z_delta += random.choice([*[0] * 9, 1])
 
 
 if __name__ == "__main__":
-    plat = Platforms()
-    [plat.add() for _ in range(100)]
+    sky = urs.Sky()
+    urs.scene.fog_density = 0.01
+
+    platforms = Platforms()
+    # Creates all the platforms
+    [platforms.add() for _ in range(100)]
+    # Colors the last one red
+    platforms.platforms[-1].color = urs.color.hex("#cb2a2a")
 
     player = FirstPersonController()
     player.speed = 40
